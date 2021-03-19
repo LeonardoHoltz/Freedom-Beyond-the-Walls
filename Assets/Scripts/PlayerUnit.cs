8aace51f12bc94ca179bb900ef7bb9699c066e63
Leonardo Holtz <leo.holtz98@gmail.com>
2021-03-19 03:18:27 -0300

    start of hud
    health bar

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -10,17 +10,33 @@ namespace FBTW.Units.Player
     public class PlayerUnit : MonoBehaviour
     {
         public NavMeshAgent navAgent;
-        Vector3 oldPos, newPos;
 
         public int foodCost;
         public int gasCost;
         public int bladeCost;
 
+        public int maxHealth = 5;
+        private int currentHealth;
+        public HealthBar healthBar;
+
+        void Start()
+        {
+            currentHealth = maxHealth;
+            healthBar.SetMaxHealth(maxHealth);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+        }
         
         public void MoveUnit(Vector3 _destination)
         {
             navAgent.SetDestination(_destination);
         }
+
+
     }
     
 }
