c646fb9a6fff5c816eccfda48fb2a2c7201c6c66
AugustoDP <34219628+AugustoDP@users.noreply.github.com>
2021-05-13 22:48:21 -0300

    Fixed minimap + animations

---
+++ Assets/Scripts/CameraPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPoint : MonoBehaviour
+{
+    GameObject sceneCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneCamera = GameObject.Find("CameraRig");
+        this.transform.SetParent(sceneCamera.transform);
+        this.transform.position = sceneCamera.transform.position; ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
