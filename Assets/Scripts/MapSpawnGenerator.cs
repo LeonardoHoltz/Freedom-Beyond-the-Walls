8502347c3bb7f9467ce404c418b91b167e16a81e
Leonardo Holtz <leo.holtz98@gmail.com>
2021-05-28 00:35:46 -0300

    Spawn city and other things in the correct area

--- Assets/Scripts/MapSpawnGenerator.cs
+++ Assets/Scripts/MapSpawnGenerator.cs
@@ -2,101 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace FBTW.SpawnGenerator
+public class MapSpawnGenerator : MonoBehaviour
 {
-    public class MapSpawnGenerator : MonoBehaviour
+    private int numberOfTrees = 300;
+    private int numberOfRocks = 200;
+
+    private float mapSize, mapSizeForCity; // TODO: Get size of map in the future, hardcoded right now
+
+    public List<GameObject> treesPrefabs;
+    public List<GameObject> rocksPrefabs;
+    public GameObject shiganshina, titan;
+
+    public int numberTitans = 5;
+
+    public float xCitySpawn, zCitySpawn;
+
+    public Transform parent;
+    public GameObject treeChild, rockChild;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        public static MapSpawnGenerator instance;
+        System.Random rndTree = new System.Random();
+        System.Random rndRock = new System.Random();
+        parent = GameObject.Find("Scenario").transform;
 
-        private int numberOfTrees = 300;
-        private int numberOfRocks = 200;
+        mapSize = 250;
+        mapSizeForCity = 200;
 
-        private float mapSize, mapSizeForCity; // TODO: Get size of map in the future, hardcoded right now
+        float xSpawn, zSpawn;
 
-        public List<GameObject> treesPrefabs;
-        public List<GameObject> rocksPrefabs;
-        public GameObject shiganshina, titan;
+        // Shiganshina Spawn:
 
-        public int numberTitans = 5;
+        xCitySpawn = Random.Range(mapSizeForCity * -1, mapSizeForCity);
+        zCitySpawn = Random.Range(mapSizeForCity * -1, mapSizeForCity);
 
-        public float xCitySpawn, zCitySpawn;
+        Instantiate(shiganshina, new Vector3(xCitySpawn, 0, zCitySpawn), Quaternion.identity).transform.SetParent(parent);
 
-        public Transform parent;
-        public GameObject treeChild, rockChild;
+        // Titans Spawn:
 
-        // Start is called before the first frame update
-        void Start()
+        bool insideCity = false;
+
+        for (int i = 0; i < numberTitans; i++)
         {
-            System.Random rndTree = new System.Random();
-            System.Random rndRock = new System.Random();
-            parent = GameObject.Find("Scenario").transform;
-
-            mapSize = 5 * GameObject.Find("Plane").transform.localScale.x;
-            mapSizeForCity = 3.5f * GameObject.Find("Plane").transform.localScale.x;
-
-            float xSpawn, zSpawn;
-
-            // Shiganshina Spawn:
-
-            xCitySpawn = Random.Range(mapSizeForCity * -1, mapSizeForCity);
-            zCitySpawn = Random.Range(mapSizeForCity * -1, mapSizeForCity);
-
-            Instantiate(shiganshina, new Vector3(xCitySpawn, 0, zCitySpawn), Quaternion.identity).transform.SetParent(parent);
-
-            // Titans Spawn:
-
-            bool insideCity = false;
-
-            for (int i = 0; i < numberTitans; i++)
+            do
             {
-                do
+                Debug.Log("Aaaaaaaaaa");
+                xSpawn = Random.Range(mapSize * -1, mapSize);
+                zSpawn = Random.Range(mapSize * -1, mapSize);
+                if ((xSpawn > xCitySpawn - 40f && xSpawn < xCitySpawn + 40f) && (zSpawn > zCitySpawn - 40f && zSpawn < zCitySpawn + 40f))
                 {
-                    xSpawn = Random.Range(mapSize * -1, mapSize);
-                    zSpawn = Random.Range(mapSize * -1, mapSize);
-                    if ((xSpawn > xCitySpawn - 50f && xSpawn < xCitySpawn + 50f) && (zSpawn > zCitySpawn - 50f && zSpawn < zCitySpawn + 50f))
-                    {
-                        insideCity = true;
-                    }
+                    insideCity = true;
+                }
 
-                } while (Physics.CheckSphere(new Vector3(xSpawn, 1f, zSpawn), 0.75f) || insideCity);
-                insideCity = false;
-                Instantiate(titan, new Vector3(xSpawn, 0, zSpawn), Quaternion.identity).transform.SetParent(GameObject.Find("Titans").transform);
-            }
-
-            for (int i = 0; i < numberOfTrees; i++)
-            {
-                do
-                {
-                    xSpawn = Random.Range(mapSize * -1, mapSize);
-                    zSpawn = Random.Range(mapSize * -1, mapSize);
-
-                } while (Physics.CheckSphere(new Vector3(xSpawn, 1f, zSpawn), 0.75f));
-
-                treeChild = Instantiate(treesPrefabs[rndTree.Next(treesPrefabs.Count)], new Vector3(xSpawn, 0, zSpawn), Quaternion.identity);
-                treeChild.layer = 8;
-                treeChild.transform.SetParent(parent);
-            }
-
-            for (int i = 0; i < numberOfRocks; i++)
-            {
-                do
-                {
-                    xSpawn = Random.Range(mapSize * -1, mapSize);
-                    zSpawn = Random.Range(mapSize * -1, mapSize);
-
-                } while (Physics.CheckSphere(new Vector3(xSpawn, 1f, zSpawn), 0.75f));
-
-                rockChild = Instantiate(rocksPrefabs[rndRock.Next(rocksPrefabs.Count)], new Vector3(xSpawn, 0, zSpawn), Quaternion.identity);
-
-                rockChild.transform.SetParent(parent);
-            }
-
+            } while (Physics.CheckSphere(new Vector3(xSpawn, 1f, zSpawn), 0.75f) || insideCity);
+            insideCity = false;
+            Instantiate(titan, new Vector3(xSpawn, 0, zSpawn), Quaternion.identity).transform.SetParent(GameObject.Find("Titans").transform);
         }
 
-        // Update is called once per frame
-        void Update()
+        for (int i = 0; i < numberOfTrees; i++)
         {
+            do
+            {
+                xSpawn = Random.Range(mapSize * -1, mapSize);
+                zSpawn = Random.Range(mapSize * -1, mapSize);
 
+            } while (Physics.CheckSphere(new Vector3(xSpawn, 1f, zSpawn), 0.75f));
+
+            treeChild = Instantiate(treesPrefabs[rndTree.Next(treesPrefabs.Count)], new Vector3(xSpawn, 0, zSpawn), Quaternion.identity);
+            treeChild.layer = 8;
+            treeChild.transform.SetParent(parent);
         }
+
+        for (int i = 0; i < numberOfRocks; i++)
+        {
+            do
+            {
+                xSpawn = Random.Range(mapSize * -1, mapSize);
+                zSpawn = Random.Range(mapSize * -1, mapSize);
+
+            } while (Physics.CheckSphere(new Vector3(xSpawn, 1f, zSpawn), 0.75f));
+
+            rockChild = Instantiate(rocksPrefabs[rndRock.Next(rocksPrefabs.Count)], new Vector3(xSpawn, 0, zSpawn), Quaternion.identity);
+
+            rockChild.transform.SetParent(parent);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
